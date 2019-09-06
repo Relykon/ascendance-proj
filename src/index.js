7cbf152d402e8dea696e0ade3aeb4add3ca16ac3
Kyle Harrington <harrington.kyle@gmail.com>
2019-09-06 12:16:05 -0700

    add context API to pass firebase component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { HashRouter } from 'react-router-dom';
+
 import './index.css';
-import App from '../src/components/App/App';
-import { HashRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-const render = (Component) => {
-    ReactDOM.render(
-        <HashRouter>
-            <Component />
-        </HashRouter>,
-        document.getElementById('root')
-    );
-};
+import App from '../src/components/App/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
-render(App); 
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
+); 
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
