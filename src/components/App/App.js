5764a51531e87eca0f332077cda05f65e9ba77dd
Kyle Harrington <harrington.kyle@gmail.com>
2019-09-06 13:11:40 -0700

    add SignUpForm & logic to SignUp Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 // import { v4 } from 'uuid';
 import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
+import LandingPage from '../Landing/Landing';
+import SignUpPage from '../SignUp/SignUp';
+import SignInPage from '../SignIn/SignIn';
+import PasswordForgetPage from '../PasswordForget/PasswordForget';
+import HomePage from '../Home/Home';
+import AccountPage from '../Account/Account';
+import AdminPage from '../Admin/Admin';
 
-
+import * as ROUTES from '../../constants/Routes';
 
 const App = () => (
   <BrowserRouter>
   <Header />
-  <Switch>
-    <Route exact path="/" render={() => <Landing /> } />
-    <Route component={Error404} />
-  </Switch>
+    <div>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
   </BrowserRouter>
 )
 
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/Routes';
+
+const SignUpPage = () => (
+    <div>
+        <h1>Sign Up</h1>
+        <SignUpForm />
+    </div>
+);
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    error: null,
+};
+
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = event => {
+
+    }
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+        const {
+            username,
+            email,
+            password,
+            passwordConfirm,
+            error,
+        } = this.state;
+
+        const isInvalid = 
+            password !== passwordConfirm ||
+            password === '' ||
+            email === '' ||
+            username === '';
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input 
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="User Name"
+                />
+                <input 
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input 
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input 
+                    name="passwordConfirm"
+                    value={passwordConfirm}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Sign Up
+                </button>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        );
+    }
+}
+
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+
+export default SignUpPage;
+export { SignUpForm, SignUpLink };
