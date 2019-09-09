005baf73f9155130a3139cc158cd9f8285c57055
Kyle Harrington <harrington.kyle@gmail.com>
2019-09-09 15:12:01 -0700

    add conditionally rendered Nav Links for authUser

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/Routes';
 
-const Navigation = () => (
-<div>
+const Navigation = ({ authUser }) => (
+    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
     <ul>
-        <li>
-            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-        </li>
         <li>
             <Link to={ROUTES.LANDING}>Landing</Link>
         </li>
@@ -21,10 +20,29 @@ const Navigation = () => (
             <Link to={ROUTES.ACCOUNT}>Account</Link>
         </li>
         <li>
+            <SignOutButton />
+        </li>        
+    </ul>
+);
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+    </ul>
+);
+
+export default Navigation;
+
+    <ul>
+        <li>
+            <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+        </li>
+        <li>
             <Link to={ROUTES.ADMIN}>Admin</Link>
         </li>
     </ul>
-</div>
-)
-
-export default Navigation;
