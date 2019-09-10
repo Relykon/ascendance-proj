792ea76e4e6a51c052bcdd46bf2f2fad9f9439e7
Kyle Harrington <harrington.kyle@gmail.com>
2019-09-10 16:36:52 -0700

    add session handling and refactor App component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import SignOutButton from '../SignOut';
+import SignOutButton from '../SignOut/SignOut';
 import * as ROUTES from '../../constants/Routes';
 
-const Navigation = ({ authUser }) => (
-    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+import { AuthUserContext } from '../Session';
+
+const Navigation = () => (
+    <div>
+        <AuthUserContext.Consumer>
+            {authUser =>
+                authUser ? <NavigationAuth /> : <NavigationNonAuth />
+            }
+        </AuthUserContext.Consumer>
+    </div>
 );
 
 const NavigationAuth = () => (
--- src/components/PasswordForget/PasswordForget.js
+++ src/components/PasswordForget/PasswordForget.js
@@ -1,1 +1,5 @@
-    
+// import REACT from 'react';
+
+
+
+// export default
