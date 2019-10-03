c62a1959e5a3be5e27e11228ee215863714607ff
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-10-03 11:26:27 -0700

    Add header styling, need to add real links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,21 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="Header">
-            <h1>Project Ascendance</h1>
+        <nav>
+        <div className="Header">        
+            <div href="#" className="logo">Project Ascendance</div>
+
+            <ul id="nav-mobile" className="menu">
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#ProjectPreview">My Projects</a></li>
+                <li><a href="#recruit">Recruit Volunteers</a></li>
+                <li><a href="#about">Our Mission</a></li>
+            </ul>
         </div>
+        </nav>
     )
 };
 
 export default Header;
+
+
